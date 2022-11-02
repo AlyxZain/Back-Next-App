2f53df3d62f87ef3633fbf00d903bb7c9420fe8d
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-11-02 01:03:25 -0500

    Allow

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 // app.use(cors(corsOptions));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://pruebasnext.vercel.app/');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
