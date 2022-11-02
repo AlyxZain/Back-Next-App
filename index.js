ec2d5fc3f00802beed811a59a6bb5d308075d991
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-11-02 00:52:03 -0500

    Allow

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://pruebasnext.vercel.app/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  // res.header('Access-Control-Allow-Origin', 'https://pruebasnext.vercel.app/');
+  // res.header('Access-Control-Allow-Credentials', 'true');
+  // res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+  // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
