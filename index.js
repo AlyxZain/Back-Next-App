d7ea63632d627d0ca0d0d6b336241f1c320e4f09
Sebastian Vargas Ramirez <AlpharGM@hotmail.com>
2022-11-03 14:46:48 -0500

    Task all

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
